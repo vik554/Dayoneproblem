2c22c1a0469c538d28e2dec78227c7293bd287da
vik554 <kumbharvikram27@gmail.com>
2023-05-09 13:28:48 +0530

    code added to UC_2

---
+++ src/Day_1Problem/DataTayp_value.java
@@ -0,0 +1,21 @@
+package Day_1Problem;
+
+public class DataTayp_value {
+    static boolean val1;
+    static double val2;
+    static float val3;
+    static int val4;
+    static long val5;
+    static String val6;
+
+    public static void main(String[] args) {
+        System.out.println("Default values.....");
+        System.out.println("boolean value = " + val1);
+        System.out.println("double value = " + val2);
+        System.out.println("float value = " + val3);
+        System.out.println("int value = " + val4);
+        System.out.println("long value = " + val5);
+        System.out.println("string value = " + val6);
+    }
+
+}
--- src/Day_1Problem/DisPlayMsg.java
+++
@@ -1,7 +0,0 @@
-package Day_1Problem;
-
-public class DisPlayMsg {
-    public static void main(String[] args) {
-        System.out.println("Wlecom to java program !");
-    }
-}
